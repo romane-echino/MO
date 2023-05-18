067d76d7bd89a664f4adf6cdf6e77dbb713f038c
DBN <dan.bourquin@lhs-sa.ch>
2023-05-18 23:40:06 +0200

    Test bodypart layer

---
+++ Assets/Scripts/Character/BodyAspect/CharacterAppeareance.cs
@@ -0,0 +1,17 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+
+namespace MO.Character.BodyAspect
+{
+    public class CharacterAppeareance : MonoBehaviour
+    {
+        public Texture2D Texture;
+
+        [ContextMenu("Test")]
+        public void Test()
+        {
+
+        }
+    }
+}
+#endif
