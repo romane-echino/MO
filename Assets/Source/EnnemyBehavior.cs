4e0d167a00a5d893dd18dbe15fa093d8a0b2a002
DBN <dan.bourquin@lhs-sa.ch>
2023-05-23 20:16:57 +0200

    Fix TD animation

--- Assets/Source/EnnemyBehavior.cs
+++ Assets/Source/EnnemyBehavior.cs
@@ -52,6 +52,7 @@ public class EnnemyBehavior : MonoBehaviour
 
     public void Die()
     {
+        animator.ResetTrigger("Hit");
         animator.SetBool("IsDead", true);
     }
 
@@ -59,5 +60,6 @@ public class EnnemyBehavior : MonoBehaviour
     {
         this.currentLife = this.maxLife;
         entityUI.UpdateLife(currentLife);
+        animator.SetBool("IsDead", false);
     }
 }
